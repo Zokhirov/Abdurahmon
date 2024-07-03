d56728b0ffee2755ffae8693b4821a8dcef573b7
Z1vran <156885631+Zokhirov@users.noreply.github.com>
2024-07-03 10:25:14 +0300

    Добавление и заполнение массива

--- Program.cs
+++ Program.cs
@@ -34,36 +34,6 @@ namespace ConsoleApp1
                 Answer2[i] = int.Parse(Console.ReadLine());
             }
             Console.ReadKey();
-            double sumDifferenceSquard = 0;
-            for (int i = 0; i < n; i++)
-            {
-                sumDifferenceSquard += Math.Pow(Answer1[i] - Answer2[i], 2);
-            }
-            double Rs = 1 - (6 * sumDifferenceSquard) / (n * (Math.Pow(n, 2) - 1));
-            Console.WriteLine($"Коэффицент ранговой корреляции Rs = {Rs}");
-            Console.WriteLine("Характер взаимосвязи по шкале Чеддока:");
-            if (Rs >= 0 && Rs < 0.2)
-            {
-                Console.WriteLine("Связь пректически отсутствует");
-            }
-            else if (Rs >= 0.2 && Rs < 0.5)
-            {
-                Console.WriteLine("Связь слабая");
-            }
-            else if (Rs >= 0.5 && Rs < 0.7)
-            {
-                Console.WriteLine("Связь средняя");
-            }
-            else if (Rs >= 0.7 && Rs < 0.95)
-            {
-                Console.WriteLine("Связь сильная");
-            }
-            else if (Rs >= 0.95 && Rs < 1)
-            {
-                Console.WriteLine("Практически функциональная завипсимость");
-            }
-            Console.WriteLine("Работу выполнил студент: Зохиров Абдурахмон Ахмаджонович");
-            Console.WriteLine("Группа: 34 ИС");
         }
     }
 }
